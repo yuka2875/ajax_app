a655c62d95b130cb894fe73ed5c155d349c60fc8
yuka2875 <yuka986532@gmail.com>
2021-10-01 20:40:35 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.order(id: "DESC")
-  end
+ def index
+   @posts = Post.order(id: "DESC")
+ end
 
   # def new
   # end
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
-  end
+ def create
+   Post.create(content: params[:content])
+   redirect_to action: :index
+ end
 end
